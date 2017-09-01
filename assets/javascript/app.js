b134a536f5b09ff7e01c6db08a488d9bf552df54
maxwellyawopoku <maxwellyawopoku@gmail.com>
2017-08-31 23:36:49 -0400

    Final4 gifhyApi

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,28 @@
       $(document).ready(function(){
 
 
-      // Initial array of movies
-      var animals = ["DOG"];
+      // Initial array of Animals
+      var animals = ["MONKEY"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      
       function displayAnimalInfo() {
-         // $("button").on("click", function() {
-            // $("#add-animal").on("click", function() {
+      
               $("#animals-view").empty();
 
 
         var animal = $(this).attr("data-name");
-        // var queryURL = "http://www.omdbapi.com/?t=" + animal + "&y=&plot=short&apikey=40e9cece";
+       
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=0149705534b84f72a74112ce7581ade1";
 
-        // Creating an AJAX call for the specific movie button being clicked
+        // Creating an AJAX call for the specific animal button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
           console.log(response);
-                // $("#add-animal").on("click", function() {
+             
 
-          // Creating a div to hold the movie
+          // Creating a div to hold the animal
           var animalDiv = $("<div class='animal'>");
 
           // Storing the rating data
@@ -49,9 +48,10 @@
 
               // Giving the image tag an src attribute of a proprty pulled off the
               // result item
-              animalImage.attr("src", results[i].images.fixed_height.url);
-              animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+              animalImage.attr("src", results[i].images.fixed_height_still.url);
               animalImage.attr("data-animate", results[i].images.fixed_height.url);
+              animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+              
 
                
 
@@ -59,11 +59,11 @@
     
               
 
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              // Appending the paragraph and animalImage we created to the "gifDiv" div we created
               gifDiv.append(p);
               gifDiv.append(animalImage);
 
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              // Prepending the gifDiv to the "#animals-view" div in the HTML
               $("#animals-view").prepend(gifDiv);
             }
           }
@@ -83,63 +83,28 @@
                 }
               });
 
-          // Creating an element to have the rating displayed
-          // var pOne = $("<p>").text("Rating: " + results.rating);
-
-          // // Displaying the rating
-          // animalDiv.append(pOne);
-
-          // // Storing the release year
-          // var released = response.Released;
-
-          // // Creating an element to hold the release year
-          // var pTwo = $("<p>").text("Released: " + released);
-
-          // // Displaying the release year
-          // animalDiv.append(pTwo);
-
-          // // Storing the plot
-          // var plot = response.Plot;
-
-          // // Creating an element to hold the plot
-          // var pThree = $("<p>").text("Plot: " + plot);
-
-          // // Appending the plot
-          // animalDiv.append(pThree);
-
-          // // Retrieving the URL for the image
-          // var imgURL = response.Poster;
-
-          // // Creating an element to hold the image
-          // var image = $("<img>").attr("src", imgURL);
-
-          // // Appending the image
-          // animalDiv.append(image);
-
-          // // Putting the entire movie above the previous 
-          // $("#animals-view").prepend(animalDiv);
-        });
+          });
 
 
         // });
 
         }
 
-      // Function for displaying movie data
+      // Function for displaying animals array data
       function renderButtons() {
 
-        // Deleting the movies prior to adding new 
+        // Deleting the animals prior to adding new 
 
         // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of animals
         for (var i = 0; i < animals.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
+          // Then dynamicaly generating buttons for each animal in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var animalName = $("<button>");
-          // Adding a class of movie to our button
+          // Adding a class of animal to our button
           animalName.addClass("animal");
           // Adding a data-attribute
           animalName.attr("data-name", animals[i]);
@@ -152,20 +117,22 @@
 
 
 
-      // This function handles events where a movie button is clicked
+      // This function handles events where a animal button is clicked
       $("#add-animal").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var animal = $("#animal-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        // Adding animal from the textbox to our array
         animals.push(animal);
 
-        // Calling renderButtons which handles the processing of our movie array
+    
+
+        // Calling renderButtons which handles the processing of our animal array
         renderButtons();
       });
 
-      // Adding a click event listener to all elements with a class of "movie"
+      // Adding a click event listener to all elements with a class of "animal"
       $(document).on("click", ".animal", displayAnimalInfo);
 
       // Calling the renderButtons function to display the intial buttons
